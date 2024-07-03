b05c8e9811bd84b97ed2031f118cb31c0f14f7ad
Nit31 <dydalinnnov31@gmail.com>
2024-07-03 16:43:23 +0300

    Saving images to files

--- ml_pipeline/prompt_to_img.py
+++ ml_pipeline/prompt_to_img.py
@@ -1,8 +1,18 @@
 import torch
 from diffusers import StableDiffusion3Pipeline
+import os
 
 
-def prompt_to_img(prompt, negative_prompt="", num_inference_steps=30, guidance_scale=8):
+def generate_images(prompt, n, request_id, negative_prompt="", num_inference_steps=30, guidance_scale=8):
+    images = []
+
+    # create an output folder
+    try:
+        os.mkdir(f"../data/images/{request_id}")
+    except FileExistsError:
+        pass
+
+    # init pipe
     pipe = StableDiffusion3Pipeline.from_pretrained(
         "stabilityai/stable-diffusion-3-medium-diffusers",
         text_encoder_3=None,
@@ -11,20 +21,25 @@ def prompt_to_img(prompt, negative_prompt="", num_inference_steps=30, guidance_s
     )
     pipe.enable_model_cpu_offload()
 
-    image = pipe(
-        prompt=prompt,
-        negative_prompt=negative_prompt,
-        num_inference_steps=num_inference_steps,
-        height=512,
-        width=512,
-        guidance_scale=guidance_scale,
-    ).images[0]
-
-    del pipe
-    return image
-
-def pipe(n, prompt, negative_prompt="", num_inference_steps=30, guidance_scale=8):
-    images = []
     for i in range(n):
-        images.append(prompt_to_img(prompt, negative_prompt, num_inference_steps, guidance_scale))
+        torch.cuda.empty_cache()
+        # generate an image and append to array
+        images.append(
+            pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                num_inference_steps=num_inference_steps,
+                height=512,
+                width=512,
+                guidance_scale=guidance_scale,
+            ).images[0]
+        )
+        images[-1].save(f"../data/images/{request_id}/{i}.png")
+
+    # clear cuda memory by deleting a pipe
+    del pipe
+
     return images
+
+
+generate_images('Dinosaur in sweater on white background', 3, 123)
